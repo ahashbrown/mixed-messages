54cf07d32a162c0247450d6f724fa9eec2df42bb
zuned11 <alex.hashemi@me.com>
2022-06-19 11:04:27 -0400

    rejoined activites, implemented activity picker

--- activities.js
+++
@@ -1,5 +0,0 @@
-//create three arrays containing the possible objectives: money-making (excluding bosses), bossing, and skilling
-
-const moneyMakers = ['Theatre of Blood','Chambers of Xeric','Pickpocketing Elves','Crafting Blood Runes','Killing Demonic Gorillas','Hallowed Sepulchre','Casting Tan Leather','Killing Lizardman Shamans','Pickpocketing Vyres','Smelting Runite','Killing Rune Dragons','Making Ranarr Potions','Hunting Black Chinchompas'];
-const bosses = ['Gauntlet','Corrupted Guantlet','Barrows','Giant Mole','Dagannoth Kings','Sarachnis','Kalphite Queen','God Wars Dungeon (Any)','Nex','Zulrah','Vorkath','Corporeal Beast','The Nightmare','Wildy'];
-const skills = ['Prayer','Magic','Runecraft','Hitpoints','Crafting','Mining','Smithing','Fishing','Cooking','Firemaking','Woodcutting','Agility','Herblore','Thieving','Fletching','Slayer','Farming','Construction','Hunter'];
--- script.js
+++ script.js
@@ -1,37 +1,59 @@
-import ./activities.js;
+//create three arrays containing the possible objectives: money-making (excluding bosses), bossing, and skilling
+
+const moneyMakers = ['Theatre of Blood','Chambers of Xeric','Pickpocketing Elves','Crafting Blood Runes','Killing Demonic Gorillas','Hallowed Sepulchre','Casting Tan Leather','Killing Lizardman Shamans','Pickpocketing Vyres','Smelting Runite','Killing Rune Dragons','Making Ranarr Potions','Hunting Black Chinchompas'];
+const bosses = ['Gauntlet','Corrupted Guantlet','Barrows','Giant Mole','Dagannoth Kings','Sarachnis','Kalphite Queen','God Wars Dungeon (Any)','Nex','Zulrah','Vorkath','Corporeal Beast','The Nightmare','Wildy'];
+const skills = ['Prayer','Magic','Runecraft','Hitpoints','Crafting','Mining','Smithing','Fishing','Cooking','Firemaking','Woodcutting','Agility','Herblore','Thieving','Fletching','Slayer','Farming','Construction','Hunter'];
 
 //helper to generate random numbers
 function randNum(max) {
-	return Math.round(Math.random()) * max;
+	return Math.round(Math.random() * max);
 }
 
 //generate an activity 
-const pickActivity (() => {
+function pickActivity() {
 	let activity = {
 		goalType: '',
 		goal: '',
-		length: 0;
+		duration: 0
 	}
 
-	// @TODO: fill in the blanks here
-
+	let newGoal = randNum(3);
+	switch (newGoal) {
+		case 1:
+			activity.goalType = 'moneyMakers'
+			activity.goal = moneyMakers[randNum(moneyMakers.length)]
+			activity.duration = randNum(2) + 1
+			break;
+		case 2:
+			activity.goalType = 'bosses'
+			activity.goal = bosses[randNum(bosses.length)]
+			activity.duration = randNum(30) + 1
+			break;
+		case 3:
+			activity.goalType = 'skills'
+			activity.goal = bosses[randNum(bosses.length)]
+			activity.duration = randNum(2) + 1
+			break;
+		default: 
+			console.log('Error!')
+	}
+	console.log(activity)
 	return activity;
-})
+}
 
 //format activity
-function formatActivitiy(activity) {
+function formatActivity(activity) {
 	// @TODO: turn an activity object into a pretty string
+	return activity;
 }
 
 //final 'main' function to be run on script execution
 function main() {
 	let responses = [];
-	//format 3 responses
 
-	for (let i = 0; i < 3; i++) responses[i] = pickActivity(randNum)
+	for (let i = 0; i < 3; i++) responses[i] = formatActivity(pickActivity())
 
-	//print 3 responses
-
+	for (const response of responses) console.log(response)
 }
 
 main()
